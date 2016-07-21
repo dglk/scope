b480c7840859a05a77472a516628f2a23e77c8dc
dglk <dglebovsky@gmail.com>
2016-07-21 15:49:45 +0300

    Basic  and  implementation

--- index.js
+++ index.js
@@ -2,7 +2,27 @@
   'use strict';
 
   var Scope = function() {
+    this.$$watchers = [];
   };
-  
+
+  Scope.prototype.$watch = function(watchFn, listenerFn) {
+    this.$$watchers.push({
+      watchFn: watchFn,
+      listenerFn: listenerFn || function() {},
+      last: undefined
+    });
+  };
+
+  Scope.prototype.$digest = function() {
+    var scope = this;
+    _.forEach(this.$$watchers, function(watcher) {
+      var current = watcher.watchFn(scope);
+      if (current !== watcher.last) {
+        watcher.listenerFn(scope);
+        watcher.last = current;
+      }
+    });
+  };
+
   window.Scope = Scope;
 })();
